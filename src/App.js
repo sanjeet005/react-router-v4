42ed1d8fbcee076cc3872d4a9dd2cc8cad4edb0d
Sanjeet Kumar <sanjeet.kumar@zylotech.com>
2017-08-30 11:55:35 +0530

    wip

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { 
+import {
   BrowserRouter as Router,
   Route,
   Link,
@@ -48,7 +48,7 @@ class Form extends React.Component{
 
 const Links = () => (
  <div className="list-group">
-    <NavLink className="list-group-item" exact activeClassName="active" to="/" >Home</NavLink>
+   <NavLink className="list-group-item" exact activeClassName="active" to="/" >Home</NavLink>
    <NavLink className="list-group-item" activeClassName="active" to="/about" >About</NavLink>
    <NavLink className="list-group-item" activeClassName="active" to="/content" >Content</NavLink>
    <NavLink className="list-group-item" activeClassName="active" to="/form" >Form</NavLink>
@@ -65,33 +65,34 @@ const Content = () => (
 
 const ContentDetails = (props) => (
   <div>
-  { props.match.params.contentName ? 
+  { props.match.params.contentName ?
     <div>
-    <img src={'http://lorempixel.com/400/200/'+props.match.params.contentName+'/1/'} /> 
-    </div> 
-    : 
+      <img src={'http://lorempixel.com/400/200/'+props.match.params.contentName+'/1/'} />
+    </div>
+    :
     null
    }
    </div>
 )
 
+
 const App = () => (
   <Router>
   <div className="row">
-    <section className="col-sm-4"> 
+    <section className="col-sm-4">
       <Links />
     </section>
-    
+
     <section  className="col-sm-8">
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/content" component={Content} />
-      <Route path="/form" component={Form} />
-      <Route  render={() => <h1>Not Founddsadasdasd</h1>} />
-    </Switch>  
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/content" component={Content} />
+        <Route path="/form" component={Form} />
+        <Route component ={NotFound} />
+      </Switch>
     </section>
-    
+
     </div>
   </Router>
 )
